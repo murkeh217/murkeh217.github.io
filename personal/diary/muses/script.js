665c6e2efde422789f7979d0d7a021b2857700c2
Murtaza Kanorwala <36036034+murkeh217@users.noreply.github.com>
2026-04-02 19:52:04 +0530

    updated muses

--- personal/diary/muses/script.js
+++ personal/diary/muses/script.js
@@ -6,36 +6,34 @@ gsap.registerPlugin(CustomEase);
 CustomEase.create("hop", "0.9, 0, 0.1, 1");
 // Project titles
 const items = [
-  "Chromatic Loopscape",
-  "Solar Bloom",
-  "Neon Handscape",
-  "Echo Discs",
-  "Void Gaze",
-  "Gravity Sync",
-  "Heat Core",
-  "Fractal Mirage",
-  "Nova Pulse",
-  "Sonic Horizon",
-  "Dream Circuit",
-  "Lunar Mesh",
-  "Radiant Dusk",
-  "Pixel Drift",
-  "Vortex Bloom",
-  "Shadow Static",
-  "Crimson Phase",
-  "Retro Cascade",
-  "Photon Fold",
-  "Zenith Flow"
-];
+  "2B",
+  "Psylocke",
+  "Weed",
+  "Disha Patani",
+  "Angie Miller",
+  "Yuu Shinoda",
+  "Whiskey",
+  "Rum & Cola",
+  "Eve",
+  "Sweetie Fox",
+  "Shen Qiao",
+  "Wine",
+  "Mojito"];
 // Image URLs - replace with your actual image URLs
 const imageUrls = [
-  "https://cdn.cosmos.so/0f164449-f65e-4584-9d62-a9b3e1f4a90a?format=jpeg",
-  "https://cdn.cosmos.so/74ccf6cc-7672-4deb-ba13-1727b7dc6146?format=jpeg",
-  "https://cdn.cosmos.so/2f49a117-05e7-4ae9-9e95-b9917f970adb?format=jpeg",
-  "https://cdn.cosmos.so/7b5340f5-b4dc-4c08-8495-c507fa81480b?format=jpeg",
-  "https://cdn.cosmos.so/f733585a-081e-48e7-a30e-e636446f2168?format=jpeg",
-  "https://cdn.cosmos.so/47caf8a0-f456-41c5-98ea-6d0476315731?format=jpeg",
-  "https://cdn.cosmos.so/f99f8445-6a19-4a9a-9de3-ac382acc1a3f?format=jpeg"
+  "2bsexy.jpg",
+  "psylockesexy.webp",
+  "weed.webp",
+  "dishasexy.avif",
+  "angiemiller.jpg",
+  "yuushinoda.jpg",
+  "whiskey.webp",
+  "rumcola.webp",
+  "eve.webp",
+  "swfx.webp",
+  "shenqiao.jpg",
+  "wine.webp",
+  "mojito.jpg"
 ];
 const container = document.querySelector(".container");
 const canvas = document.getElementById("canvas");
